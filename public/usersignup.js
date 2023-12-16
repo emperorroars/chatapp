6fcda9834ec2543fd7ceb234a3ee03bb6cb8b3cd
emperorroars <daemosares@gmail.com.com>
2023-12-15 18:38:22 -0800

    signup

---
+++ public/usersignup.js
@@ -0,0 +1,28 @@
+let signupbutton=document.getElementById("submit")
+signupbutton.addEventListener("click",details)
+const url='http://localhost:4000'
+function details(event)
+{
+    event.preventDefault();
+    let name=document.getElementById("name").value
+    let email=document.getElementById("email").value
+    let password=document.getElementById("password").value
+    let phoneno=document.getElementById("phone").value
+    const user={
+        name:name,
+        password:password,
+        email:email,
+        phone:phoneno
+    }
+    console.log(user)
+    axios.post(`/user/signup`, user)
+    .then((response)=>{
+        console.log("posted")
+        alert(response.data.message);
+    })
+    .catch((err)=>{
+       const failuremessage=document.getElementById("failuremessage")
+       failuremessage.innerHTML="User Already exists"
+  
+    })
+}

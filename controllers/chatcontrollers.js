9ff41350a680d368cdc9a3955995463003ebf289
emperorroars <daemosares@gmail.com.com>
2023-12-16 23:40:55 -0800

    getting chat

--- controllers/chatcontrollers.js
+++ controllers/chatcontrollers.js
@@ -1,15 +1,15 @@
 const Chat=require('../models/chat')
 const User = require('../models/user');
 const sequelize = require('../utils/database');
-exports.add = (req, res, next) => {
+exports.add = (req, res) => {
     console.log("adding")
       console.log("see the user now", req.user);
       console.log("see the body now", req.body);
-      console.log(req.body.description);
       console.log("see the id now", req.user.id);
   
        Chat.create({
         message: req.body.message,
+        userId:req.user.id
         })
         .then(()=>{
             res.status(200).json({ success: true, message: "Success" });
@@ -21,4 +21,15 @@ exports.add = (req, res, next) => {
       return res.status(500).json({ success: false, error: err });
     })
   };
+  exports.get=(req,res)=>{
+    Chat.findAll()
+      .then(chats => {
+        res.json(chats);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: 'An error occurred while fetching expenses' });
+      });
+  };
+
   
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,18 @@
+window.addEventListener("DOMContentLoaded",()=>{
+  console.log("reloaded")
+  const token = localStorage.getItem('token');
+  axios.get("/chat/get", {
+    headers: { "Authorization": token }})
+  .then((response) => {
+    console.log(" i am the user",response)
+    for(var i=0;i<response.data.length;i++)
+    renderExpenseList(response.data[i])
+    
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+})
 var SendButton = document.getElementById('send')
 SendButton.addEventListener("click",addchat)
 
@@ -25,3 +40,29 @@ function addchat(event) {
       console.log("FAILED")
     })
     }}
+    function renderExpenseList(chat) {
+      console.log("render is working")
+      //const expenses = JSON.parse(localStorage.getItem('expenses')) || [];
+      const itemsList = document.getElementById('chats');
+     // itemsList.innerHTML = ''; // Clear previous list items
+    
+      //expenses.forEach((expense, index) => {
+        const listItem = document.createElement('li');
+        listItem.className = 'list-group-item';
+        listItem.textContent = `${chat.message}`;
+    
+        /*const deleteButton = document.createElement('button');
+        deleteButton.className = 'btn btn-danger btn-sm float-right delete';
+        deleteButton.textContent = 'X';
+        deleteButton.addEventListener('click', () => deleteExpense(expense,listItem));
+    
+        const editButton = document.createElement('button');
+        editButton.className = 'btn btn-primary btn-sm float-right edit';
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', () => editExpense(expense,listItem));
+    
+        listItem.appendChild(deleteButton);
+        listItem.appendChild(editButton);*/
+        itemsList.appendChild(listItem);
+      //});
+    }
